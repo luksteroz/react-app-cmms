3990ac7d042c6e2d9e69b29ae0e8200b2160e6f4
Marek Sieńkowski <mareksienkowski.90@gmail.com>
2020-08-21 00:54:18 +0200

    new style

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,28 @@
-import React, { Component } from 'react';
-import './App.css';
-import AppNavbar from './AppNavbar';
-import { Link } from 'react-router-dom';
-import { Button, Container } from 'reactstrap';
+import React, { Component } from "react";
+import "./App.css";
+import AppNavbar from "./AppNavbar";
+import { Link } from "react-router-dom";
+import { Button, Container } from "reactstrap";
+import "./index.css";
 
 class Home extends Component {
-    render() {
-        return (
-            <div>
-                <AppNavbar/>
-                <Container fluid>
-                    <Button color="link"><Link to="/accidents">Manage Accidents</Link></Button>
-                    <Button color="link"><Link to="/users">Manage Users</Link></Button>
-                </Container>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <AppNavbar />
+        <Container fluid>
+          <Button color="link">
+            {" "}
+            <Link to="/accidents"> Manage Accidents </Link>
+          </Button>
+          <Button color="link">
+            {" "}
+            <Link to="/users"> Manage Users </Link>
+          </Button>
+        </Container>{" "}
+      </div>
+    );
+  }
 }
 
 export default Home;
